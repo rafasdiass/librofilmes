fe1613192caa362935570c701887e9206b6930e0
Rafael Dias <rafaeldias@192.168.100.2>
2025-03-26 07:36:49 -0300

    fix: navbar funcionando

--- src/app/shared/movie-preview-card/movie-preview-card.component.ts
+++ src/app/shared/movie-preview-card/movie-preview-card.component.ts
@@ -10,10 +10,6 @@ import { CommonModule } from '@angular/common';
 import { Movie } from '../models/movie.model';
 import { MatCardModule } from '@angular/material/card';
 
-/**
- * Card que exibe um preview do filme.
- * Esse componente se limita à exibição dos dados e não gerencia a navegação.
- */
 @Component({
   selector: 'app-movie-preview-card',
   standalone: true,
@@ -23,32 +19,14 @@ import { MatCardModule } from '@angular/material/card';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MoviePreviewCardComponent implements OnChanges {
-  /**
-   * Propriedade de entrada. Recebe o objeto do filme e dispara a atualização dos signals.
-   */
   @Input() public movie: Movie | null = null;
-
-  /**
-   * Signal que armazena o objeto do filme local.
-   */
   private readonly localMovieSignal = signal<Movie | null>(null);
-
-  /**
-   * Signal que armazena a URL do poster.
-   */
   public readonly posterUrlSignal = signal<string>('');
 
-  /**
-   * Getter para acessar o valor atual da URL do poster.
-   */
   public get posterUrl(): string {
     return this.posterUrlSignal();
   }
 
-  /**
-   * Método do ciclo de vida chamado sempre que houver alteração em alguma propriedade de entrada.
-   * Atualiza os signals de filme e URL do poster com base na nova entrada.
-   */
   public ngOnChanges(changes: SimpleChanges): void {
     if (changes['movie']) {
       try {
@@ -77,9 +55,6 @@ export class MoviePreviewCardComponent implements OnChanges {
     }
   }
 
-  /**
-   * Método getter para retornar o objeto do filme armazenado localmente.
-   */
   public getLocalMovie(): Movie | null {
     return this.localMovieSignal();
   }
